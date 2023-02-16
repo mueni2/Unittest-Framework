24837d69b38af12f4eb842800efa6b6c3ece7ff9
mueni2 <anmomoan@gmail.com>
2023-02-16 14:51:34 +0000

    adding basic files

---
+++ evens.py
@@ -0,0 +1,13 @@
+def even_number_of_evens(numbers):
+    """
+    Should Raise a TypeError if a list in not passed into the function
+    error message: "A list was not passed into the function"
+    if the list is empty it will return False
+    if the number of even numbers is odd - return False
+    if the numner of even numbers is even - return True
+    """
+
+    return None
+
+if __name__ == "__main__":
+    print(even_number_of_evens(5))
---
+++ test_evens.py
@@ -0,0 +1,10 @@
+import unittest
+from evens import even_number_of_evens
+
+
+class TestEvens(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
